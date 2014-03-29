ff475dc2559793f4325d7bd5e00f9a4c5c17eaf4
Aghasy Poghosyan <agasy18@gmail.com>
2014-03-29 15:28:20 +0400

    closing implemsented

---
+++ CSharp/seuaLabs/WpfLabs/WpfApplication6/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace WpfApplication6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            if (MessageBox.Show("Close ?", "Hello", MessageBoxButton.YesNo, MessageBoxImage.Question) ==
+                MessageBoxResult.Yes) return;
+            e.Cancel = true;
+        }
+    }
+}
